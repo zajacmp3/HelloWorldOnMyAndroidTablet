5b033bc80648f9b865f68c2c066d781eb1d48ef5
Mateusz Zając <deejay.zajac@gmail.com>
2012-10-09 23:07:05 +0200

    Initial Release

---
+++ src/com/zajacmp3/hello/MainActivity.java
@@ -0,0 +1,21 @@
+package com.zajacmp3.hello;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import java.util.*;
+import java.io.*;
+
+@SuppressWarnings("unused")
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+    }
+}
